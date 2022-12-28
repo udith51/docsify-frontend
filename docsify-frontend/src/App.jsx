0009744ea4174d0b61afd0b2c722cfe8ca577d4c
Udith Shyamsukha <udithshyamsukha@gmail.com>
2022-12-29 01:59:44 +0530

    Home page updates

--- docsify-frontend/src/App.jsx
+++ docsify-frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Home from './pages/home/Home'
+import Login from './pages/auth/Login'
+import Register from './pages/auth/Register'
 
 const App = () => {
     return (
         <div className="app">
-            <Home />
+            {/* <Home /> */}
+            <Login env="Patient" />
+            {/* <Register env="Patient" /> */}
         </div>
     )
 }
--- docsify-frontend/src/components/topbar/Box.jsx
+++ docsify-frontend/src/components/topbar/Box.jsx
@@ -3,9 +3,6 @@ import './box.css'
 
 export default function Box(props) {
     const { image, content, bg } = props
-    const mystyle = { bg }
-    console.log(bg);
-
     return (
         <div className="box" style={bg}>
             <img src={image} alt="" />
